eff3998c0cd968d5823820be0ecc9d82d327399b
Luiz Henrique Dalri Fifolato <ldalri1999@gmail.com>
2021-06-22 20:40:14 -0300

    Adicionar arquivos de projeto.

---
+++ Prova_Auth_22_06_21/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prova_Auth_22_06_21.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Prova_Auth_22_06_21/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Prova_Auth_22_06_21.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
